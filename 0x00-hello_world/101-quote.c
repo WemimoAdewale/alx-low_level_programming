f80bcb6812ace63bc605567bef546c6b01004cac
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-06-30 21:52:17 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+ * main - prints exactly and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19 . 
+ *   followed by a new line, to the standard error. 
+ *   Return: Always 0 (success)
+ */
 int main(void)
 {
-printf("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19, \n");
-return (0);
+write(2, "and that piece of art is usefull\" -Dora Korpar,2015-10-19\n", 59);
+return (1);
 }
 
