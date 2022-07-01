60f6bde138d576dac9e54ae31e048ba57a907428
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-07-01 07:58:52 -0700

    task4 corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/*
+ * main - main is the entry point
+ * return - Sucess
+ * function is to print a sentence
+ */
 int main(void)
 {
 puts(" \"Programming is like building a multilingual puzzle");
