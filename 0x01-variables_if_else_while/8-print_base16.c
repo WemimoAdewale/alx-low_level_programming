fda76e2c369229717ccb1e4a56d4b3eeb60ae615
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-07-02 02:37:49 -0700

    corrected

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * main : prints all letters in hexadecimal
- *
- * return (0);
- */
-nt main(void)
+* main - Entry point
+*
+* Return:Always 0 - success
+*/
+int main(void)
 {
-/**
- * hexadecimal
- * 0 1 2 3 4 5 6 7 8 9 a b c d e f
- **/
 char ch;
 int n;
 
@@ -27,3 +22,4 @@ putchar(ch);
 putchar(10);
 
 return (0);
+}
