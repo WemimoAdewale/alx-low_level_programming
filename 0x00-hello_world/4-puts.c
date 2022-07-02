dcc2bb3a26cc35dd4ac9587f2e589f7665d5ba63
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-07-02 14:09:48 -0700

    corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - program to print...
- * return - sucess
- */
+* main - Entry point
+*
+* Return:Always 0 -sucess
+*/
 int main(void)
 {
-puts(" \"Programming is like building a multilingual puzzle,");
-return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
