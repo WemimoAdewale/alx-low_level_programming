12796de56070070dafa9800074fd9f47fdbee9e4
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-07-02 14:26:42 -0700

    corrected

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints exactly and that piece of art is useful"
- * - Dora Korpar, 2015-10-19 . 
- *   followed by a new line, to the standard error. 
- *   Return: Always 0 (success)
- */
+* main -Entry point
+* Return:Always 0 -sucess
+*/
 int main(void)
 {
-write(2, "and that piece of art is usefull\" -Dora Korpar,2015-10-19\n", 59);
+write(2, "and that piece of art is usefull\" -Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
-
