6cfaf48d0da0ff94b1001e7bdadf1e550aba079d
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-07-07 03:01:15 -0700

    task2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 void positive_o_negative(int i);
-int largest_number(int a, int b int c);
+int largest_number(int a, int b, int c);
 int convey_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
