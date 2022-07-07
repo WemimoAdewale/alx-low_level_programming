db9a0281c4529ba20e3effcd865495dbd9e5b194
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-07-07 14:52:15 -0700

    task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 /**
 * print_remaining_days - :takes a date and prints out how many days are there in a leap year
 * Description: prints the date
