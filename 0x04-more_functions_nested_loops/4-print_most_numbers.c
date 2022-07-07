81a227eae2599550ac593b3ad8e64d1a2c15225c
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-07-07 07:18:20 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_most_numbers - prints most numbers except 2 and 3
+* Description:prints
+* Return:0
+*/
+void print_most_numbers(void)
+{
+int a = 0;
+
+while (a <= 9)
+{
+if (a != 2 && a != 4)
+{
+_putchar(a + '0');
+}
+a++;
+}
+_putchar('\n');
+
