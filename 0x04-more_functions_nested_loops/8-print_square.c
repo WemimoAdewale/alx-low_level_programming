f592f1fef1d4b8f96ba38e373e68684a7f5ea494
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-07-08 00:38:10 -0700

    task8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,23 +1,21 @@
 #include "main.h"
 /**
-* print_diagonal - print diagonal lines
+* print_square - print diagonal lines
 * @n:number of /to be printed
 * Return:void
 */
-void print_diagonal(int n)
+void print_square(int n)
 {
 int i = 0, ii;
 
 while (i < n && n > 0)
 {
 ii = 0;
-
-while (ii < i)
+while (ii < n)
 {
-_putchar(' ');
+_putchar('#');
 ii++;
 }
-_putchar('\\');
 _putchar('\n');
 i++;
 }
