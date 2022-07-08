b9a86adbe4e85737458879e22b2e40df32a79787
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-07-07 22:49:50 -0700

    main.h error corrected

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 void positive_o_negative(int i);
 int largest_number(int a, int b, int c);
-int convey_day(int month, int day);
+int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
 #endif    /*MAIN_H*/
