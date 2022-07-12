a2ce68fb4089a08588028e2a0af546db22c3c9bc
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-07-12 01:38:50 -0700

    task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* rev_string - prints a string in reverse
+* @s: the ised string to reverse
+* Return:0;
+*/
+void rev_string(char *s)
+{
+int len, i, half;
+char temp;
+
+for (len = 0; s[len] != '\0'; len++)
+;
+i = 0;
+half = len / 2;
+
+while (half--)
+{
+temp = s[len - i - 1];
+s[len - i - 1] = s[i];
+s[i] = temp;
+i++;
+}
+}
