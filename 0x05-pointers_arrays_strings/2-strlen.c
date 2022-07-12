2efe52b24d56bed245b95fbd7d662fb892e82ecd
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-07-12 01:18:51 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _strlen - checks the length of a string
+* @s: string to check
+* Return: length od string
+*/
+int _strlen(char *s)
+{
+int length = 0;
+
+while (s[length])
+	length++;
+return (length);
+}
