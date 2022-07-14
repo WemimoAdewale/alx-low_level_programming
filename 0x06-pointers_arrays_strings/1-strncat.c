72396f0d932730e5d131f3067c3c0ea653b7b0a6
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-07-14 08:10:05 -0700

    file present

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+* _strncat - concatenates two strings
+* @dest:string to be appended upon
+* @src: destination string
+* @n:the number of bytes from src
+* Return:pointerresult
+*/
+
