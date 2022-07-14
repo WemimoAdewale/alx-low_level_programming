4f44fe512ef689beececc5904df47ff0feacf8ef
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-07-14 08:34:05 -0700

    cleared betty error

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,7 +3,7 @@
 * reverse_array - Reverses the content of an array of integers
 * @a:array of integers to ne reveresd
 * @n:array of numbers in the array
-*/ 
+*/
 void reverse_array(int *a, int n)
 {
 	int tmp, index;
