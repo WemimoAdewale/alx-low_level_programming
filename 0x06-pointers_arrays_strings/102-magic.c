ab87a80bb3dd85880afe3d1ed33d9c72444a194e
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-07-15 14:20:05 -0700

    new edit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -13,10 +13,11 @@ int main(void)
  * remember
  * you are not allowed to use p
  * - you are not allowed to modify p
- *   - only one statement
- *   - you are not allowed to code anything else  */
+ * - only one statement
+ * - you are not allowed to code anything else than this line of code
+ */
 	*(p + 5) = 98;
-  /* ...so that this prints 98\n */
+/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
