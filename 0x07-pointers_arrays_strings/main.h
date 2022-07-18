6580c1fe7c304f847306051015cc9162cd2fae4b
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-07-18 05:28:21 -0700

    task corrected

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,4 +12,5 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
 #endif
