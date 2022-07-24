e178f90344e059e4086d59f4b1fae1efbae61a21
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-07-24 03:32:11 -0700

    task 0ahain

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,10 +1,12 @@
-#include <unistd>
+#include <unistd.h>
 /**
-* _putchar - writes character c to stdout
-* @c: character to print
-* Return:0 on sucess
+* _putchar - writes the character c to stdout
+* @c:the character to print
+* Return:Always 0 on sucess
+* on error, -1 is returned and erno is set appropriately
 */
-int _putchar(char c)
+int _putchar (char c)
 {
-	return (write(1, &c, 1));
+return (write(1, &1, 1));
 }
+
