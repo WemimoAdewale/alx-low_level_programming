b4936077707f9680b7a4e9a6589cbaa10a15a0c0
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-08-01 08:03:06 -0700

    corrected4

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,5 +1,11 @@
-#ifndef SUM
-#define SUM
+#ifndef SUM_H
+#define SUM_H
+/**
+* File: 4-sum.h
+* Desc: Header file that defines a function-like macro SUM(x, y)
+* @x:macro parameter
+* @y:macro parameter
+*/
 
 #define SUM(x, y) ((x) + (y))
 #endif
