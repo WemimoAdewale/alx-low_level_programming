8aba1c91f58b4577848220e1e907d988c6035f87
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-08-01 07:32:44 -0700

    task 0

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,8 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+/**
+* File: 0-object_like_macro.h
+* Desc: Header file that defines a macro named SIZE as an abbreviation for 1024
+*/
+#define SIZE 1024
+#endif /* SIZE define*/
