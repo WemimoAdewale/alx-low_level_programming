6d1218b0d6781240bc9d736db2f94fc6c717a2f7
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-08-02 11:18:08 -0700

    task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,7 +17,7 @@ struct dog
 	char *owner;
 }
 /**
-* dog_t - typedef dor dog struct* 
+* dog_t - typedef dor dog struct
 */
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
