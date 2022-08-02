8deb6acba3b2df23d997cc677f3366db8b3adc24
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-08-02 11:05:00 -0700

    my dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* File: dog.h
+* Desc: Header file that defines a new type struct dog.
+*/
+/**
+* struct dog - A new type describing a dog.
+* @name: the name of yhe dog
+* @age:the age of the fog
+* @owner:the owner of the dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+/**
+* dog_t - typedef dor dog struct* 
+*/
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
