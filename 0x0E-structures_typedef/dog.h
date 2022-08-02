a1d07adb864a40560e3edaffd72eed6ab594407c
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-08-02 11:30:52 -0700

    adjusted dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,11 +15,14 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
 /**
 * dog_t - typedef dor dog struct
 */
-typedef struct dog dog_t;
+
+typedef  struct  dog  dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
